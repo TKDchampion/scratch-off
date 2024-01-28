133290f8e66b8b4bdc6f2cbaf041f664bed08507
giant <giant.wang@dotdot.cc>
2024-01-28 17:44:26 +0800

    try to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   basePath: "/scratch-off",
-  //   output: "export", // <=== enables static exports
-  //   reactStrictMode: true,
+  basePath: "/scratch-off",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
